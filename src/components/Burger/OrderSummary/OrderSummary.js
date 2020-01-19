154f3fd04a016e6ccfe619b70b6d498a5e649932
Gerardo Tzoc <gath2-96@hotmail.com>
2020-01-19 00:21:36 -0600

    Added a basic toolbar to the app ... to be continued

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -20,6 +20,7 @@ import Button from '../../UI/Button/Button';
         <ul>
             {ingredientSummary}
         </ul>
+    <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
         <p>Continue to Checkout?</p>
         <Button btnType = "Danger" clicked = {props.purchaseCanceled}>CANCEL</Button>
         <Button btnType = "Success" clicked = {props.purchaseContinue}>CONTINUE</Button>
